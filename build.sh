7bac7e798040938cb37707c3dc7a90b84c7d7a7d
Raul Cumplido <raulcumplido@gmail.com>
2014-10-20 16:51:23 +0100

    Fix Flask run app

--- build.sh
+++ build.sh
@@ -13,7 +13,6 @@ docker build -t django:latest ./django-docker/
 docker run --link db:db django python manage.py migrate 
 docker run -d -p 8000:8000 --link db:db --name web_backend -v `pwd`/django-docker/:/code django python manage.py runserver 0.0.0.0:8000
 docker build -t flask:latest ./flask-docker/
-docker run -p 5000:5000 --name search_backend flask &
-sleep 3
+docker run -d -p 5000:5000 --name search_backend flask
 docker build -t nginx:latest ./nginx-docker/
 docker run -d -p 80:80 --link web_backend:web_backend --link search_backend:search_backend --name nginx nginx
